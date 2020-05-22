c2f7313795ee99c5477f1918d43b3df2270800d4
Dominik <megakopec@gmail.com>
2020-05-22 17:19:02 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  rules: {
+    'spaced-comment': 0,
+    'import/extensions': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'no-plusplus': 0,
+    'prefer-template': 0,
+    'class-methods-use-this': 0,
+    'linebreak-style': 0,
+    'arrow-parens': 0,
+  },
+}
