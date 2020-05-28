f428eb3b39ca2e4a4a008a19ab63b6bbdf603ad6
Omegakopec <megakopec@gmail.com>
2020-05-28 12:05:32 -0400

    remove player counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import './App.css';
 import Header from './components/Header/Header';
 import FactionList from './components/FactionList/FactionList';
@@ -7,16 +6,11 @@ import PlayerSelect from './components/PlayerSelect/PlayerSelect';
 import Results from './components/Results/Results';
 
 function App() {
-  const counter = useSelector((state) => state.playerCount);
 
   return (
     <div className="App">
       <Header />
       <div className="body-wrapper">
-        <h4>
-          Players:
-          {counter}
-        </h4>
         <FactionList />
         <PlayerSelect />
         <Results />
