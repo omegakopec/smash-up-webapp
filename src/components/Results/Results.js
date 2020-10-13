65108fdec13e1140af961c10f9c687d37ea50a70
omegakopec <megakopec@gmail.com>
2020-10-13 14:31:20 -0400

    reducers: generate randomized results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import ResultCard from '../ResultCard/ResultCard';
 import './Results.css';
 
-const generateCards = (players) => {
+const generateCards = (players, selectedFactions) => {
+  selectedFactions.sort(() => 0.5 - Math.random())
   const cardList = [];
-  for (let i = 1; i <= players; i++) {
+  for (let i = 0; i < players; i += 1) {
     cardList.push(
       <li className="card">
-        <ResultCard name={`Player #${i}`} faction1="Pirates" faction2="Aliens" />
+        <ResultCard name={`Player #${i + 1}`} faction1={selectedFactions[i+i]} faction2={selectedFactions[i + i + 1]} />
       </li>,
     );
   }
   return cardList;
 };
 
-const Results = () => {
-  const playerTotal = useSelector((state) => state.playerCount);
-
+const Results = (props) => {
+  let players = props.playerTotal
+  let selections = props.selectedFactions
   return (
     <div className="results-wrapper">
       <div className="table">
         <ul id="card-list">
-          {generateCards(playerTotal)}
+          {generateCards(players, selections)}
         </ul>
       </div>
     </div>
   );
 };
 
-export default Results;
+const mapStateToProps = (state) => ({
+  playerTotal: state.playerCount,
+  selectedFactions: state.selectedFactions,
+});
+
+export default connect(mapStateToProps)(Results);
