491f7c84d2cd53b664cdaf558edfbbc763972d41
omegakopec <megakopec@gmail.com>
2020-10-14 10:51:58 -0400

    Results: add prop-type validation

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,38 +1,42 @@
+/* eslint-disable react/forbid-prop-types */
+
 import React from 'react';
 import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 import ResultCard from '../ResultCard/ResultCard';
 import './Results.css';
 
 const generateCards = (players, selectedFactions) => {
-  selectedFactions.sort(() => 0.5 - Math.random())
+  selectedFactions.sort(() => 0.5 - Math.random());
   const cardList = [];
   for (let i = 0; i < players; i += 1) {
     cardList.push(
       <li className="card">
-        <ResultCard name={`Player #${i + 1}`} faction1={selectedFactions[i+i]} faction2={selectedFactions[i + i + 1]} />
+        <ResultCard name={`Player #${i + 1}`} faction1={selectedFactions[2*i]} faction2={selectedFactions[2*i + 1]} />
       </li>,
     );
   }
   return cardList;
 };
 
-const Results = (props) => {
-  let players = props.playerTotal
-  let selections = props.selectedFactions
-  return (
-    <div className="results-wrapper">
-      <div className="table">
-        <ul id="card-list">
-          {generateCards(players, selections)}
-        </ul>
-      </div>
+const Results = ({ playerTotal, selectedFactions }) => (
+  <div className="results-wrapper">
+    <div className="table">
+      <ul id="card-list">
+        {generateCards(playerTotal, selectedFactions)}
+      </ul>
     </div>
-  );
-};
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   playerTotal: state.playerCount,
   selectedFactions: state.selectedFactions,
 });
+
+Results.propTypes = {
+  playerTotal: propTypes.number.isRequired,
+  selectedFactions: propTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(Results);
