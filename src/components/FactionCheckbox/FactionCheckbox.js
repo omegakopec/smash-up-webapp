2933c1ad8e4dd01860605b76eb358e0ab8885a52
omegakopec <megakopec@gmail.com>
2020-10-14 16:28:13 -0400

    FactionCheckbox: add logic to update selectedFactionsCount

--- src/components/FactionCheckbox/FactionCheckbox.js
+++ src/components/FactionCheckbox/FactionCheckbox.js
@@ -1,13 +1,19 @@
 /* eslint react/forbid-prop-types: 0 */ // --> OFF
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import './FactionCheckbox.css';
 
-const FactionCheckbox = ({ name, label, factions }) => {
+const FactionCheckbox = ({
+  name, label, factions, selectedFactions,
+}) => {
   const [checked, setChecked] = useState(false);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'UPDATE', payload: selectedFactions.length });
+  });
 
   return (
     <div className="faction-checkbox">
@@ -31,10 +37,15 @@ const FactionCheckbox = ({ name, label, factions }) => {
   );
 };
 
+const mapStateToProps = (state) => ({
+  selectedFactions: state.selectedFactions,
+});
+
 FactionCheckbox.propTypes = {
   name: propTypes.string.isRequired,
   label: propTypes.string.isRequired,
   factions: propTypes.array.isRequired,
+  selectedFactions: propTypes.array.isRequired,
 };
 
-export default FactionCheckbox;
+export default connect(mapStateToProps)(FactionCheckbox);
