bfc0a384bc885a08385de34e5a6a8d73e98ec2a8
omegakopec <megakopec@gmail.com>
2020-11-05 16:21:28 -0500

    ResultsCard: Import faction modal

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,7 +1,6 @@
-/* eslint import/no-dynamic-require: 0 */ // --> OFF
-
 import React from 'react';
 import propTypes from 'prop-types';
+import FactionModal from '../FactionModal/FactionModal';
 import './ResultCard.css';
 
 const ResultsCard = ({ name, faction1, faction2 }) => (
@@ -9,31 +8,9 @@ const ResultsCard = ({ name, faction1, faction2 }) => (
     <h4 id="card-title">
       {name}
     </h4>
-    <div className="faction-wrapper">
-      {faction1}
-      <div className="logo-container">
-        <img
-          src={require(`../../assets/FactionLogos/${faction1.replace(/ /g, '_')}.png`)}
-          alt="faction1"
-          id="factionLogo"
-          height="60"
-          width="45"
-        />
-      </div>
-    </div>
+    <FactionModal faction={faction1} />
     <p>&</p>
-    <div className="faction-wrapper">
-      {faction2}
-      <div className="logo-container">
-        <img
-          src={require(`../../assets/FactionLogos/${faction2.replace(/ /g, '_')}.png`)}
-          alt="faction1"
-          id="factionLogo"
-          height="60"
-          width="45"
-        />
-      </div>
-    </div>
+    <FactionModal faction={faction2} />
   </div>
 );
 
