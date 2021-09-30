07a56fdf6089c4881a8257f69255c9c6bff362d7
omegakopec <megakopec@gmail.com>
2021-09-29 23:17:12 -0400

    eslint: add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'spaced-comment': 0,
+    'import/extensions': 0,
+    "import/prefer-default-export": 0,
+    'react/jsx-filename-extension': 0,
+    "react/jsx-props-no-spreading": "off",
+    "jsx-a11y/click-events-have-key-events": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'no-plusplus': 0,
+    'no-undef': 0,
+    'prefer-template': 0,
+    'class-methods-use-this': 0,
+    'linebreak-style': 0,
+    'arrow-parens': 0,
+    "global-require": 0
+  },
+};
