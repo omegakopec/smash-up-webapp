c6504d9b9f51b13cab7aeac19d8aa05bc351089f
omegakopec <megakopec@gmail.com>
2021-11-21 22:16:55 -0500

    index: use BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-/* eslint-disable */
-
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+
 import App from './App';
 import './index.scss';
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-render(
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
